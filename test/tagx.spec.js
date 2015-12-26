3e136ed262146412d4313117e50773f9e4d0b80e
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-26 09:03:35 +0100

    Rewrite test with lab and code

--- test/tagx.spec.js
+++ test/tagx.spec.js
@@ -1,171 +1,217 @@
 /* Copyright (c) 2013 Richard Rodger */
-"use strict";
+'use strict';
 
+var Lab = require('lab');
+var Code = require('code');
 
-if( typeof tagx === 'undefined' ) {
-  var tagx = require('..')
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+var tagx = require('..')
+
+function s(t) {
+  return t.toString();
 }
 
+describe('tagx', function () {
 
-
-
-describe('tagx', function() {
-  
-  it('happy', function() {
-    expect( ''+['red','green','blue'] ).toBe( ''+tagx('red, green, blue') )
+  it('happy', function (done) {
+    expect(s(tagx('red, green, blue'))).to.equal(s(['red', 'green', 'blue']))
+    done()
   })
 
 
-  it('split', function() {
-    
-    expect( ''+[] ).toBe( ''+tagx() )
-    expect( ''+[] ).toBe( ''+tagx('') )
+  describe('split', function () {
 
-    expect( ''+['a'] ).toBe( ''+tagx('a') )
-    expect( ''+['a'] ).toBe( ''+tagx('a') )
-    expect( ''+['a'] ).toBe( ''+tagx(' a') )
-    expect( ''+['a'] ).toBe( ''+tagx('a ') )
-    expect( ''+['a'] ).toBe( ''+tagx(' a ') )
-    expect( ''+['a'] ).toBe( ''+tagx(',a') )
-    expect( ''+['a'] ).toBe( ''+tagx('a,') )
-    expect( ''+['a'] ).toBe( ''+tagx(',a,') )
-    expect( ''+['a'] ).toBe( ''+tagx(', a') )
-    expect( ''+['a'] ).toBe( ''+tagx('a ,') )
-    expect( ''+['a'] ).toBe( ''+tagx(', a ,') )
-    expect( ''+['a'] ).toBe( ''+tagx(' ,a') )
-    expect( ''+['a'] ).toBe( ''+tagx('a, ') )
-    expect( ''+['a'] ).toBe( ''+tagx(' ,a, ') )
+    it('empty', function (done) {
+      var empty = s([])
+      expect(s(tagx())).to.equal(empty)
+      expect(s(tagx(''))).to.equal(empty)
+      done()
+    })
 
-    expect( ''+['aa'] ).toBe( ''+tagx('aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx('aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx(' aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx('aa ') )
-    expect( ''+['aa'] ).toBe( ''+tagx(' aa ') )
-    expect( ''+['aa'] ).toBe( ''+tagx(',aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx('aa,') )
-    expect( ''+['aa'] ).toBe( ''+tagx(',aa,') )
-    expect( ''+['aa'] ).toBe( ''+tagx(', aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx('aa ,') )
-    expect( ''+['aa'] ).toBe( ''+tagx(', aa ,') )
-    expect( ''+['aa'] ).toBe( ''+tagx(' ,aa') )
-    expect( ''+['aa'] ).toBe( ''+tagx('aa, ') )
-    expect( ''+['aa'] ).toBe( ''+tagx(' ,aa, ') )
+    it('single char array', function (done) {
+      [
+        tagx('a'),
+        tagx('a'),
+        tagx(' a'),
+        tagx('a '),
+        tagx(' a '),
+        tagx(',a'),
+        tagx('a,'),
+        tagx(',a,'),
+        tagx(', a'),
+        tagx('a ,'),
+        tagx(', a ,'),
+        tagx(' ,a'),
+        tagx('a, '),
+        tagx(' ,a, ')].forEach(function (it) {
+        expect(s(it)).to.equal(s(['a']));
+      })
 
-    expect( ''+['a','b'] ).toBe( ''+tagx('a,b') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a,b,') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b ') )
-    expect( ''+['a','b'] ).toBe( ''+tagx(',a,b,') )
-    expect( ''+['a','b'] ).toBe( ''+tagx(' a b ') )
+      done()
+    })
 
-    expect( ''+['a','b','c'] ).toBe( ''+tagx('a,b,c') )
-    expect( ''+['a','b','c'] ).toBe( ''+tagx('a b c') )
+
+    it('two char array', function (done) {
+      [tagx('aa'),
+        tagx('aa'),
+        tagx(' aa'),
+        tagx('aa '),
+        tagx(' aa '),
+        tagx(',aa'),
+        tagx('aa,'),
+        tagx(',aa,'),
+        tagx(', aa'),
+        tagx('aa ,'),
+        tagx(', aa ,'),
+        tagx(' ,aa'),
+        tagx('aa, '),
+        tagx(' ,aa, ')].forEach(function (it) {
+        expect(s(it)).to.equal(s(['aa']));
+      })
+
+      done()
+    })
+
+    it('two elem array', function (done) {
+      [
+        tagx('a,b'),
+        tagx('a b'),
+        tagx('a,b,'),
+        tagx('a b '),
+        tagx(',a,b,'),
+        tagx(' a b ')].forEach(function (it) {
+        expect(s(it)).to.equal(s(['a', 'b']));
+      })
+
+      done()
+    })
+
+
+    it('three elem array', function (done) {
+      [
+        tagx('a,b,c'),
+        tagx('a b c')].forEach(function (it) {
+        expect(s(it)).to.equal(s(['a', 'b', 'c']));
+      })
+
+      done()
+    })
 
   })
 
 
-  it('append', function(){
-    expect( ''+['a','b'] ).toBe( ''+tagx('a','b') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b','') )
-    expect( ''+['a','b','c'] ).toBe( ''+tagx('a b','c') )
-    expect( ''+['a','b','c','d'] ).toBe( ''+tagx('a b','c d') )
+  it('append', function (done) {
+    expect(s(tagx('a', 'b'))).to.equal(s(['a', 'b']))
+    expect(s(tagx('a b', ''))).to.equal(s(['a', 'b']))
+    expect(s(tagx('a b', 'c'))).to.equal(s(['a', 'b', 'c']))
+    expect(s(tagx('a b', 'c d'))).to.equal(s(['a', 'b', 'c', 'd']))
+    done()
   })
 
 
-  it('remove', function(){
-    expect( ''+['a'] ).toBe( ''+tagx('a !b') )
-    expect( ''+['a'] ).toBe( ''+tagx('!b a') )
+  it('remove', function (done) {
+    expect(s(tagx('a !b'))).to.equal(s(['a']))
+    expect(s(tagx('!b a'))).to.equal(s(['a']))
 
-    expect( ''+['a'] ).toBe( ''+tagx('a b !b') )
-    expect( ''+['a'] ).toBe( ''+tagx('a b','!b') )
-    expect( ''+['a'] ).toBe( ''+tagx('a b c','!b !c') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b','c','!c') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b','c d','!d !c') )
+    expect(s(tagx('a b !b'))).to.equal(s(['a']))
+    expect(s(tagx('a b', '!b'))).to.equal(s(['a']))
+    expect(s(tagx('a b c', '!b !c'))).to.equal(s(['a']))
+    expect(s(tagx('a b', 'c', '!c'))).to.equal(s(['a', 'b']))
+    expect(s(tagx('a b', 'c d', '!d !c'))).to.equal(s(['a', 'b']))
 
-    expect( ''+['a'] ).toBe( ''+tagx('a !b b') )
-    expect( ''+['a'] ).toBe( ''+tagx('a !b','b') )
-    expect( ''+['a'] ).toBe( ''+tagx('a !b !c','b c') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b','!c','c') )
-    expect( ''+['a','b'] ).toBe( ''+tagx('a b','!c !d','d c') )
+    expect(s(tagx('a !b b'))).to.equal(s(['a']))
+    expect(s(tagx('a !b', 'b'))).to.equal(s(['a']))
+    expect(s(tagx('a !b !c', 'b c'))).to.equal(s(['a']))
+    expect(s(tagx('a b', '!c', 'c'))).to.equal(s(['a', 'b']))
+    expect(s(tagx('a b', '!c !d', 'd c'))).to.equal(s(['a', 'b']))
+    done()
   })
 
 
-  it('fixed', function(){
+  it('fixed', function (done) {
     var t0 = tagx({})
-    expect( ''+['a'] ).toBe( ''+t0('a') )
-    expect( ''+['a','b'] ).toBe( ''+t0('a','b') )
-    expect( ''+['a','b'] ).toBe( ''+t0('a b') )
+    expect(s(t0('a'))).to.equal(s(['a']))
+    expect(s(t0('a', 'b'))).to.equal(s(['a', 'b']))
+    expect(s(t0('a b'))).to.equal(s(['a', 'b']))
 
-    var t1 = tagx({},'')
-    expect( ''+['a'] ).toBe( ''+t1('a') )
-    expect( ''+['a','b'] ).toBe( ''+t1('a','b') )
-    expect( ''+['a','b'] ).toBe( ''+t1('a b') )
+    var t1 = tagx({}, '')
+    expect(s(t1('a'))).to.equal(s(['a']))
+    expect(s(t1('a', 'b'))).to.equal(s(['a', 'b']))
+    expect(s(t1('a b'))).to.equal(s(['a', 'b']))
 
-    var t2 = tagx({},'x')
-    expect( ''+['x','a'] ).toBe( ''+t2('a') )
-    expect( ''+['x','a','b'] ).toBe( ''+t2('a','b') )
-    expect( ''+['x','a','b'] ).toBe( ''+t2('a b') )
+    var t2 = tagx({}, 'x')
+    expect(s(t2('a'))).to.equal(s(['x', 'a']))
+    expect(s(t2('a', 'b'))).to.equal(s(['x', 'a', 'b']))
+    expect(s(t2('a b'))).to.equal(s(['x', 'a', 'b']))
 
-    var t3 = tagx({},'x','y')
-    expect( ''+['x','y','a'] ).toBe( ''+t3('a') )
-    expect( ''+['x','y','a','b'] ).toBe( ''+t3('a','b') )
-    expect( ''+['x','y','a','b'] ).toBe( ''+t3('a b') )
+    var t3 = tagx({}, 'x', 'y')
+    expect(s(t3('a'))).to.equal(s(['x', 'y', 'a']))
+    expect(s(t3('a', 'b'))).to.equal(s(['x', 'y', 'a', 'b']))
+    expect(s(t3('a b'))).to.equal(s(['x', 'y', 'a', 'b']))
 
-    var t4 = tagx({},'x y')
-    expect( ''+['x','y','a'] ).toBe( ''+t4('a') )
-    expect( ''+['x','y','a','b'] ).toBe( ''+t4('a','b') )
-    expect( ''+['x','y','a','b'] ).toBe( ''+t4('a b') )
+    var t4 = tagx({}, 'x y')
+    expect(s(t4('a'))).to.equal(s(['x', 'y', 'a']))
+    expect(s(t4('a', 'b'))).to.equal(s(['x', 'y', 'a', 'b']))
+    expect(s(t4('a b'))).to.equal(s(['x', 'y', 'a', 'b']))
+    done()
   })
 
 
-  it('expand', function(){
-    var t1 = tagx({expand:{u:'x'}})
-    expect( ''+['a'] ).toBe( ''+t1('a') )
-    expect( ''+['x'] ).toBe( ''+t1('u') )
-    expect( ''+['a','x'] ).toBe( ''+t1('a u') )
-    expect( ''+['x','a'] ).toBe( ''+t1('u a') )
-    expect( ''+['a','x','b'] ).toBe( ''+t1('a','u','b') )
-    expect( ''+['a','x','b'] ).toBe( ''+t1('a u b') )
+  it('expand', function (done) {
+    var t1 = tagx({expand: {u: 'x'}})
+    expect(s(t1('a'))).to.equal(s(['a']))
+    expect(s(t1('u'))).to.equal(s(['x']))
+    expect(s(t1('a u'))).to.equal(s(['a', 'x']))
+    expect(s(t1('u a'))).to.equal(s(['x', 'a']))
+    expect(s(t1('a', 'u', 'b'))).to.equal(s(['a', 'x', 'b']))
+    expect(s(t1('a u b'))).to.equal(s(['a', 'x', 'b']))
 
-    var t2 = tagx({expand:{u:'x y'}})
-    expect( ''+['a'] ).toBe( ''+t2('a') )
-    expect( ''+['x','y'] ).toBe( ''+t2('u') )
-    expect( ''+['a','x','y'] ).toBe( ''+t2('a u') )
-    expect( ''+['x','y','a'] ).toBe( ''+t2('u a') )
-    expect( ''+['a','x','y','b'] ).toBe( ''+t2('a','u','b') )
-    expect( ''+['a','x','y','b'] ).toBe( ''+t2('a u b') )
+    var t2 = tagx({expand: {u: 'x y'}})
+    expect(s(t2('a'))).to.equal(s(['a']))
+    expect(s(t2('u'))).to.equal(s(['x', 'y']))
+    expect(s(t2('a u'))).to.equal(s(['a', 'x', 'y']))
+    expect(s(t2('u a'))).to.equal(s(['x', 'y', 'a']))
+    expect(s(t2('a', 'u', 'b'))).to.equal(s(['a', 'x', 'y', 'b']))
+    expect(s(t2('a u b'))).to.equal(s(['a', 'x', 'y', 'b']))
 
-    var t3 = tagx({expand:{u:'x y',v:'z'}})
-    expect( ''+['a'] ).toBe( ''+t3('a') )
-    expect( ''+['x','y','z'] ).toBe( ''+t3('u v') )
-    expect( ''+['a','x','y','z'] ).toBe( ''+t3('a u v') )
-    expect( ''+['x','y','a','z'] ).toBe( ''+t3('u a v') )
-    expect( ''+['a','x','y','z','b'] ).toBe( ''+t3('a','u','v','b') )
-    expect( ''+['a','x','y','z','b'] ).toBe( ''+t3('a u v b') )
+    var t3 = tagx({expand: {u: 'x y', v: 'z'}})
+    expect(s(t3('a'))).to.equal(s(['a']))
+    expect(s(t3('u v'))).to.equal(s(['x', 'y', 'z']))
+    expect(s(t3('a u v'))).to.equal(s(['a', 'x', 'y', 'z']))
+    expect(s(t3('u a v'))).to.equal(s(['x', 'y', 'a', 'z']))
+    expect(s(t3('a', 'u', 'v', 'b'))).to.equal(s(['a', 'x', 'y', 'z', 'b']))
+    expect(s(t3('a u v b'))).to.equal(s(['a', 'x', 'y', 'z', 'b']))
+    done()
   })
 
 
-  it('expand-remove', function(){
-    var t1 = tagx({expand:{u:'x !y'}})
-    expect( ''+['a','x'] ).toBe( ''+t1('a u') )
-    expect( ''+['a','x'] ).toBe( ''+t1('a y u') )
-    expect( ''+['a'] ).toBe( ''+t1('a !x u') )
+  it('expand-remove', function (done) {
+    var t1 = tagx({expand: {u: 'x !y'}})
+    expect(s(t1('a u'))).to.equal(s(['a', 'x']))
+    expect(s(t1('a y u'))).to.equal(s(['a', 'x']))
+    expect(s(t1('a !x u'))).to.equal(s(['a']))
+    done()
   })
 
 
-  it('expand-depth', function(){
-    var t1 = tagx({expand:{u:'v',v:'w'}})
-    expect( ''+['a','w'] ).toBe( ''+t1('a u') )
+  it('expand-depth', function (done) {
+    var t1 = tagx({expand: {u: 'v', v: 'w'}})
+    expect(s(t1('a u'))).to.equal(s(['a', 'w']))
 
     // cycles
-    var t2 = tagx({expand:{u:'v',v:'u'}})
-    expect( ''+['a','u'] ).toBe( ''+t2('a u') )
+    var t2 = tagx({expand: {u: 'v', v: 'u'}})
+    expect(s(t2('a u'))).to.equal(s(['a', 'u']))
+    done()
   })
 
 
-  it('case', function(){
-    var t1 = tagx({case:false})
-    expect( ''+['a','bb'] ).toBe( ''+t1('a A Bb') )
+  it('case', function (done) {
+    var t1 = tagx({case: false})
+    expect(s(t1('a A Bb'))).to.equal(s(['a', 'bb']))
+    done()
   })
 
 })
